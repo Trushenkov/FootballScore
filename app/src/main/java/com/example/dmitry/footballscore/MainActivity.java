c3c11defe5c1fd1e158630d5cb25a45d951b93ab
Trushenkov Dmitry <diman28051999@gmail.com>
2018-10-01 14:46:40 +0300

    refactoring

--- app/src/main/java/com/example/dmitry/footballscore/MainActivity.java
+++ app/src/main/java/com/example/dmitry/footballscore/MainActivity.java
@@ -10,8 +10,9 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private Integer spartak = 0;
-
     private Integer loko = 0;
+    private TextView spartakTextView;
+    private TextView lokoTextView;
 
     private static final String TAG = "MainActivity";
 
@@ -19,42 +20,36 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        TextView spartakView = findViewById(R.id.spartak_score);
-        spartakView.setText(spartak.toString());
-
-        TextView lokoView = findViewById(R.id.loko_score);
-        lokoView.setText(loko.toString());
+        resetUI();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spartakTextView = findViewById(R.id.spartak_score);
+        lokoTextView = findViewById(R.id.loko_score);
     }
 
     @SuppressLint("SetTextI18n")
     public void onClickAddSpartak(View view) {
         spartak++;
-        TextView counterView = findViewById(R.id.spartak_score);
-        counterView.setText(spartak.toString());
+        spartakTextView.setText(spartak.toString());
     }
 
     @SuppressLint("SetTextI18n")
     public void onClickAddLokomotiv(View view) {
         loko++;
-        TextView counterView2 = findViewById(R.id.loko_score);
-        counterView2.setText(loko.toString());
+        lokoTextView.setText(loko.toString());
     }
 
     @SuppressLint("SetTextI18n")
     public void onClickButtonClear(View view) {
         spartak = 0;
         loko = 0;
-        TextView counterView = findViewById(R.id.spartak_score);
-        counterView.setText(spartak.toString());
 
-        TextView counterView2 = findViewById(R.id.loko_score);
-        counterView2.setText(loko.toString());
+        resetUI();
     }
 
     @Override
@@ -75,6 +70,13 @@ public class MainActivity extends AppCompatActivity {
             loko = savedInstanceState.getInt("loko");
         }
         Log.d(TAG, "onRestoreInstanceState");
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void resetUI() {
+        spartakTextView.setText(spartak.toString());
+        lokoTextView.setText(loko.toString());
+        Log.d(TAG, "resetUI");
     }
 
 }
